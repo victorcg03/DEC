9d98c6d81402e8b21202ab48f33b36b78b6cb49f
victorcg03 <78515761+victorcg03@users.noreply.github.com>
2024-09-26 10:44:55 +0200

    ejers

--- DWEC_DP3 - Ejercicios [5]/2/main.js
+++ DWEC_DP3 - Ejercicios [5]/2/main.js
@@ -2,18 +2,56 @@ import mostrar_menu from "./menu.js";
 let productos = [];
 let opcionElegida = mostrar_menu();
 
-switch (opcionElegida) {
-    case 1:
-        agregarProducto();
-        break;
-    default:
-        break;
+while (opcionElegida != 0) {
+    switch (opcionElegida) {
+        case 1:
+            agregar_producto();
+            break;
+        case 2:
+            eliminar_producto();
+            break;
+        default:
+            alert("La opción elegida no es valida.")
+            break;
+    }
+    opcionElegida = mostrar_menu();
 }
-
-function agregarProducto(){
+function agregar_producto(){
     let productoNuevo = [];
-    productoNuevo.nombre = prompt("Introduce el nombre del nuevo producto:");
-    productoNuevo.precio = prompt("Introduce el precio del nuevo producto:");
-    productoNuevo.stock = prompt("Introduce el stock del nuevo producto:");
-    console.table(productoNuevo);
+    productoNuevo.id = prompt("Introduce ID única del producto:");
+    while (existeProducto(productoNuevo["id"])) {
+        productoNuevo.id = prompt("¡Esa ID de producto ya esta en uso! Introduce otra diferente, por favor:");
+    }
+    productoNuevo.nombre = prompt("Introduce el nombre del producto:");
+    productoNuevo.precio = prompt("Introduce el precio del producto:");
+    productoNuevo.stock = prompt("Introduce el stock del producto:");
+    productos.push(productoNuevo);
+    alert("Producto agregado!");
+}
+function eliminar_producto(){
+    let idProducto = prompt("Introduce ID del producto que quieres borrar:");
+    if (!existeProducto(idProducto)) {
+        alert("No existe ningun producto con esa ID.");
+    }else{
+        productos = productos.filter(producto => id != idProducto);
+        alert("Producto eliminado!");
+    }
+}
+function actualizar_producto(){
+    let idProducto = prompt("Introduce ID del producto que quieres actualizar:");
+    let producto = existeProducto(idProducto);
+    if (!producto) {
+        alert("No existe ningun producto con esa ID.");
+    }else{
+        let nuevoNombre = prompt(`Introduce el nuevo nombre del producto (actual: ${producto.nombre}):`);
+        let nuevoPrecio = prompt(`Introduce el nuevo precio del producto (actual: ${producto.precio}):`);
+        let nuevoStock = prompt(`Introduce el nuevo stock del producto (actual: ${producto.stock}):`);
+        producto.nombre = nuevoNombre ? nuevoNombre : producto.nombre;
+        producto.precio = nuevoPrecio ? nuevoPrecio : producto.precio;
+        producto.stock = nuevoStock ? nuevoStock : producto.stock;
+        alert("Producto actualizado!");
+    }
+}
+function existeProducto(id){
+    return productos.find(producto => producto.id == id);
 }
--- DWEC_DP3 - Ejercicios [5]/2/menu.js
+++ DWEC_DP3 - Ejercicios [5]/2/menu.js
@@ -1,5 +1,6 @@
 export default function(){
     let menu = "¿Qué deseas hacer? \n";
+    menu+= "(0). Salir del programa \n";
     menu+= "(1). Agregar producto \n";
     menu+= "(2). Eliminar producto \n";
     menu+= "(3). Actualizar producto \n";
