89ccf42baf41a3e42c4e5669c55fb42585b5dda9
victorcg03 <78515761+victorcg03@users.noreply.github.com>
2024-09-27 08:51:53 +0200

    ejer

--- DWEC_DP3 - Ejercicios [5]/2/main.js
+++ DWEC_DP3 - Ejercicios [5]/2/main.js
@@ -10,6 +10,15 @@ while (opcionElegida != 0) {
         case 2:
             eliminar_producto();
             break;
+        case 3:
+            actualizar_producto();
+            break;
+        case 4:
+            mostrar_inventario();
+            break;
+        case 5:
+            calcular_valor_total();
+            break;
         default:
             alert("La opción elegida no es valida.")
             break;
@@ -33,7 +42,8 @@ function eliminar_producto(){
     if (!existeProducto(idProducto)) {
         alert("No existe ningun producto con esa ID.");
     }else{
-        productos = productos.filter(producto => id != idProducto);
+        let indice = productos.findIndex(prod => prod.id == idProducto);
+        productos.splice(indice, 1);
         alert("Producto eliminado!");
     }
 }
@@ -49,8 +59,26 @@ function actualizar_producto(){
         producto.nombre = nuevoNombre ? nuevoNombre : producto.nombre;
         producto.precio = nuevoPrecio ? nuevoPrecio : producto.precio;
         producto.stock = nuevoStock ? nuevoStock : producto.stock;
+        let indice = productos.findIndex(prod => prod.id == producto.id);
+        productos.splice(indice, 1, producto);
         alert("Producto actualizado!");
     }
+}
+function mostrar_inventario(){
+    if (productos.length == 0) {
+        alert("No hay productos.");
+    }else{
+        let output = "ID - NOMBRE - PRECIO - STOCK \n";
+        productos.forEach(producto => {
+            output+= `${producto.id} - ${producto.nombre} - ${producto.precio} - ${producto.stock} \n`;
+        });
+        alert(output);
+    }
+}
+function calcular_valor_total(){
+    let valor = productos.reduce((acumulador, producto) =>
+        acumulador + (parseFloat(producto.precio) * parseInt(producto.stock)), 0);
+    alert("El valor total del inventario es: " + valor)
 }
 function existeProducto(id){
     return productos.find(producto => producto.id == id);
