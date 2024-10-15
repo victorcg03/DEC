7268040b2665cf4bb16babf873b9ab47501ec88b
Victor . <victorjosecorralguillot@gmail.com>
2024-10-15 08:21:12 +0200

    calculadora

--- calculadora_corral_victor/calculadora.js
+++ calculadora_corral_victor/calculadora.js
@@ -2,65 +2,66 @@ window.addEventListener('load', function(){
     let botones = document.querySelectorAll('.boton');
     let botonPuesto = false;
     botones.forEach(boton => {
-        boton.addEventListener('mousedown', teclaPulsada);
-        boton.addEventListener('mouseup', function(){
+        boton.addEventListener('mousedown', event => {
+            boton.classList.add('sombra');
+            teclaPulsada(boton.innerText)
+        })
+        boton.addEventListener('mouseup', () => {
             boton.classList.remove('sombra');
         });
     });
-    function desactivarTeclasCalculo(){
-        let teclasCalculo = document.querySelectorAll('.calculo').forEach(tecla => {
-            tecla.removeEventListener('mousedown', teclaPulsada);
+    document.addEventListener('keydown', event => {
+        if (!isNaN(event.key) || event.key == 'Backspace' || event.key == '/' || event.key == '-' || event.key == '+' || event.key == '.' || event.key == '%' || event.key == 'Enter' || event.key == '*' || event.key == 'x'  || event.key == '('  || event.key == ')') {
+            teclaPulsada(event.key);
+        }
+    })
+    function calcularPorcentaje(expresion) {
+        return expresion.replace(/(\d+)%(\d+(\.\d+)?)/g, function(match, num1, num2){
+            return `(${num1} * (${num2} / 100))`;
         });
     }
-    function activarTeclasCalculo(){
-        let teclasCalculo = document.querySelectorAll('.calculo').forEach(tecla => {
-            tecla.addEventListener('mousedown', teclaPulsada);
-        });
-    }
-    function teclaPulsada(event){
-        event.target.classList.add('sombra');
-        let contenidoTeclaPulsada = event.target.innerText;
+    function teclaPulsada(contenidoTeclaPulsada){
         if (contenidoTeclaPulsada == 'C') {
-            punto.addEventListener('mousedown', teclaPulsada);
             pantalla.value = 0;
             botonPuesto = false;
-        } else if(contenidoTeclaPulsada == '()'){
+        } else if(contenidoTeclaPulsada == '()' || contenidoTeclaPulsada == '(' || contenidoTeclaPulsada == ')'){
             if (!isNaN(pantalla.value.charAt(pantalla.value.length-1))) {
                 pantalla.value = '(' + pantalla.value + ')';
                 botonPuesto = true;
             }
-        } else if(contenidoTeclaPulsada == '='){
-            if (!isNaN(pantalla.value.charAt(pantalla.value.length-1))) {
-                pantalla.value = eval(pantalla.value.replaceAll('x', '*').replaceAll('%', '*0.'));
+        } else if(contenidoTeclaPulsada == '=' || contenidoTeclaPulsada == 'Enter'){
+            if (!isNaN(pantalla.value.charAt(pantalla.value.length-1)) || pantalla.value.charAt(pantalla.value.length-1) == ')') {
+                pantalla.value = eval(calcularPorcentaje(pantalla.value.replaceAll('x', '*')));
+                botonPuesto = pantalla.value.includes('.');
             }
-        } else if(contenidoTeclaPulsada == '«'){
-            if (pantalla.value.substring(pantalla.value.length-1) == '.') {
-                punto.addEventListener('mousedown', teclaPulsada); 
-            }
+        } else if(contenidoTeclaPulsada == '«' || contenidoTeclaPulsada == 'Backspace'){
+            botonPuesto = !pantalla.value.substring(pantalla.value.length-1) == '.';
             pantalla.value = pantalla.value.substring(0, pantalla.value.length-1)
             if (pantalla.value == '') {
                 pantalla.value = 0;
             }
         } else if (contenidoTeclaPulsada == '.'){
-            if (!botonPuesto) {
+            if (!botonPuesto && !isNaN(pantalla.value.charAt(pantalla.value.length-1))) {
                 pantalla.value += '.';
-                punto.removeEventListener('mousedown', teclaPulsada);
                 botonPuesto = true;
             }
         }else {
-            if (pantalla.value == 0 && !pantalla.value.includes('.')) {
-                pantalla.value = contenidoTeclaPulsada;
-            } else {
-                pantalla.value += contenidoTeclaPulsada;
+            if (contenidoTeclaPulsada == '*') {
+                contenidoTeclaPulsada = 'x';
             }
-        }
-        if (contenidoTeclaPulsada == '.' && botonPuesto) {
-            desactivarTeclasCalculo();
-        }else if(event.target.classList.contains('calculo') && contenidoTeclaPulsada != '.'){
-            desactivarTeclasCalculo();
-            botonPuesto = false;
-        } else{
-            activarTeclasCalculo();
+            if (isNaN(contenidoTeclaPulsada) && botonPuesto && pantalla.value.charAt(pantalla.value.length-1) != '.') {
+                botonPuesto = false;
+            }
+            
+            if ((isNaN(contenidoTeclaPulsada) && (!isNaN(pantalla.value.charAt(pantalla.value.length-1)) || pantalla.value.charAt(pantalla.value.length-1) == ')')) || (!isNaN(contenidoTeclaPulsada) && pantalla.value.charAt(pantalla.value.length-1) != ')')) {
+                if (pantalla.value == 0 && !pantalla.value.includes('.')) {
+                    if (!isNaN(contenidoTeclaPulsada)) {
+                        pantalla.value = contenidoTeclaPulsada;
+                    }
+                } else {
+                    pantalla.value += contenidoTeclaPulsada;
+                }
+            }
         }
     }
 });
