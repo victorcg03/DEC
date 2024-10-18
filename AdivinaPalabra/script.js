1bce585c026b3686aa7ac5ca1d426301b1b59a72
victorcg03 <78515761+victorcg03@users.noreply.github.com>
2024-10-18 08:32:30 +0200

    .

--- AdivinaPalabra/script.js
+++ AdivinaPalabra/script.js
@@ -1,7 +1,7 @@
-const abecedarioEspañol = ['a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z', 'Space'];
+const abecedarioEspañol = ['a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z'];
 let teclasPulsadas = [];
-let palabraRandom = 'Hola Mundo';
-let oportunidadLetras = 5;
+let palabraRandom;
+let oportunidadLetras = 15;
 window.addEventListener('load', ()=>{
     abecedarioEspañol.forEach(letra => {
         teclado.innerHTML += `<div class="tecla" id="${letra}">${letra}</div>`
@@ -9,12 +9,23 @@ window.addEventListener('load', ()=>{
     boton.addEventListener('click', function () {
         tarjeta.style.display = 'none';
         document.querySelector('main').classList.remove('opacidad');
+        palabraAdivinar.innerText = "";
+        palabraRandom = peliculasFamosasEsp[getRandomIntInclusive(0, peliculasFamosasEsp.length-1)];
+        console.log(palabraRandom);
+        for (let i = 0; i < palabraRandom.length; i++) {
+            let charActual = palabraRandom.toLowerCase().charAt(i);
+            console.log(charActual);
+            if (charActual == " ") {
+                console.log("espacio");
+                palabraAdivinar.innerText += " ";
+            } else if (!abecedarioEspañol.includes(charActual)) {
+                palabraAdivinar.innerText += charActual;
+            } else {
+                palabraAdivinar.innerText += '_';
+            }
+        }
         habilitarEventos();
     });
-    
-    for (let i = 0; i < palabraRandom.length; i++) {
-        palabraAdivinar.innerText += '_';
-    }
 });
 
 function teclaHover(event) {
@@ -26,9 +37,6 @@ function teclaPulsada(event){
         let tecla = document.getElementById(event.key);
         if (tecla == null) {
             tecla = event.target;
-        }
-        if (event.key == ' ') {
-            tecla = document.getElementById('Space');
         }
         if (!teclasPulsadas.includes(tecla.innerText)) {
             tecla.classList.add('pulsada');
@@ -59,10 +67,6 @@ function teclaLevantada(event){
 }
 
 function comprobarLetra(letra) {
-    if (letra == 'Space') {
-        letra = ' ';
-    }
-
     if (palabraRandom.toLowerCase().includes(letra)) {
         deshabilitarEventos();
         let textoActual = palabraAdivinar.innerText.split('');
@@ -74,16 +78,22 @@ function comprobarLetra(letra) {
                 }, i * 100);
             }
         }
+        setTimeout(() => {
+            if (palabraAdivinar.innerText.toLowerCase() == palabraRandom.toLowerCase()) {
+                partidaGanada.style.display = 'flex';
+            }else{
+                habilitarEventos();
+            }
+        }, palabraRandom.length * 100);
     } else {
         oportunidadLetras--;
+        if (oportunidadLetras == 0) {
+            deshabilitarEventos()
+            partidaPerdida.style.display = 'flex';
+        }
         oportunidadesLetras.innerText = oportunidadLetras + ' letras restantes';
     }
-    if (palabraAdivinar.innerText.toLowerCase() == palabraRandom.toLowerCase()) {
-        partidaGanada.style.display = 'flex';
-    }
-    setTimeout(() => {
-        habilitarEventos();
-    }, palabraRandom.length * 100);
+
 }
 
 function habilitarEventos(){
@@ -106,3 +116,112 @@ function deshabilitarEventos(){
     document.removeEventListener('keydown', teclaPulsada);
     document.removeEventListener('keyup', teclaLevantada);
 }
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // El máximo es inclusivo y el mínimo es inclusivo
+}
+const peliculasFamosasEsp = [
+    "El Padrino",
+    "Sueño de Fuga",
+    "Tiempos Violentos",
+    "El Caballero de la Noche",
+    "Forrest Gump",
+    "El Origen",
+    "El Club de la Pelea",
+    "Matrix",
+    "El Señor de los Anillos: La Comunidad del Anillo",
+    "Star Wars: Una Nueva Esperanza",
+    "El Señor de los Anillos: El Retorno del Rey",
+    "Interestelar",
+    "Star Wars: El Imperio Contraataca",
+    "El Silencio de los Inocentes",
+    "La Lista de Schindler",
+    "Milagros Inesperados",
+    "Volver al Futuro",
+    "Gladiador",
+    "El Rey León",
+    "Titanic",
+    "Parque Jurásico",
+    "Los Infiltrados",
+    "Rescatando al Soldado Ryan",
+    "El Gran Truco",
+    "Corazón Valiente",
+    "El Sexto Sentido",
+    "Los Miserables",
+    "La Vida es Bella",
+    "El Pianista",
+    "El Lobo de Wall Street",
+    "Django Sin Cadenas",
+    "Bastardos sin Gloria",
+    "Los Juegos del Hambre",
+    "Avatar",
+    "Cisne Negro",
+    "El Gran Hotel Budapest",
+    "El Laberinto del Fauno",
+    "La La Land",
+    "Bohemian Rhapsody",
+    "El Código Da Vinci",
+    "El Perfume: Historia de un Asesino",
+    "12 Años de Esclavitud",
+    "Roma",
+    "El Hijo de la Novia",
+    "Relatos Salvajes",
+    "La Mujer Maravilla",
+    "El Señor de los Anillos: Las Dos Torres",
+    "Rocky",
+    "Rápidos y Furiosos",
+    "Búsqueda Implacable",
+    "Hombres de Negro",
+    "Harry Potter y la Piedra Filosofal",
+    "Harry Potter y las Reliquias de la Muerte",
+    "Piratas del Caribe: La Maldición del Perla Negra",
+    "El Diablo Viste a la Moda",
+    "El Planeta de los Simios",
+    "Jumanji",
+    "Terminator 2: El Juicio Final",
+    "Indiana Jones: En Busca del Arca Perdida",
+    "Mujer Bonita",
+    "El Diario de una Pasión",
+    "Los Otros",
+    "El Exorcista",
+    "Psicosis",
+    "Scarface: El Precio del Poder",
+    "Lo que el Viento se Llevó",
+    "Cantando Bajo la Lluvia",
+    "Los Siete Samuráis",
+    "Amélie",
+    "El Niño con el Pijama de Rayas",
+    "El Gran Escape",
+    "Los Cazafantasmas",
+    "La Dama y el Vagabundo",
+    "Blanca Nieves y los Siete Enanos",
+    "Cenicienta",
+    "La Sirenita",
+    "Aladdín",
+    "El Libro de la Selva",
+    "E.T., El Extraterrestre",
+    "Buscando a Nemo",
+    "Toy Story",
+    "Toy Story 3",
+    "Up: Una Aventura de Altura",
+    "Coco",
+    "Frozen: Una Aventura Congelada",
+    "Maléfica",
+    "Intensamente",
+    "Zootopia",
+    "Ratatouille",
+    "El Viaje de Chihiro",
+    "Akira",
+    "La Tumba de las Luciérnagas",
+    "Mi Vecino Totoro",
+    "Ghost in the Shell",
+    "Cómo Entrenar a tu Dragón",
+    "Shrek",
+    "Kung Fu Panda",
+    "Los Increíbles",
+    "El Fantasma de la Ópera",
+    "El Señor de los Anillos: Las Dos Torres",
+    "El Hobbit: Un Viaje Inesperado"
+  ];
+  
