74c811c61812611060ae16317d509cb2f0ff43f7
Victor . <victorjosecorralguillot@gmail.com>
2024-10-29 02:42:02 +0100

    .

--- JuegoCartas/script.js
+++ JuegoCartas/script.js
@@ -52,10 +52,80 @@ let cartas = [
     { nombre: "ReinaEspadas", pos1: -1624.6, pos2: 690 },
     { nombre: "ReyEspadas", pos1: -1772.3, pos2: 690 }
 ];
+window, addEventListener('load', habilitarEventoBoton);
+let vidasRestantes;
+let cartaClicada1;
+let parejasAdivinadas;
+let numParejas;
+function habilitarEventoBoton(){
+    botonJugar.addEventListener("click", function(){
+        menu.style.display = "none";
+        vidasRestantes = 5;
+        cartaClicada1 = null;
+        parejasAdivinadas = 0;
+        numParejas = document.getElementById('dificultad').value;
+        let cartasMostrar = generarCartas(numParejas);
+        imprimirCartas(cartasMostrar);
+    });
+}
+function finPartida(haGanado){
+    let cartas = document.querySelectorAll('.carta');
+    cartas.forEach(carta=>{
+        carta.remove();
+    });
+    menuFinal.style.display = "flex";
+    menuFinal.style.backgroundColor = haGanado ? "green" : "red";
+    menuFinal.innerHTML = `<p>Has ${haGanado ? "ganado!" : "perdido..."}<p/>
+                                        <button id="botonVolverJugar">Volver a jugar</button>`
+}
+function click(event) {
+    event.target.classList.add('clicada');
+    setTimeout(() => {
+        event.target.classList.remove('oculta');
+    }, 350);
 
-window, addEventListener('load', () => {
-    let numCartas = 20;      
-    let body = document.querySelector('body');
+    if (cartaClicada1 === null) {
+        cartaClicada1 = event.target;
+    } else {
+        let cartaClicada2 = event.target;
+        if (cartaClicada1.dataset.carta === cartaClicada2.dataset.carta) {
+            cartaClicada1 = null;
+            parejasAdivinadas++;
+            if (parejasAdivinadas == numParejas) {
+                finPartida(true);
+            }
+        } else {
+            vidasRestantes--;
+            if(vidasRestantes == 0){
+                setTimeout(() => {
+                    finPartida(false);
+                },1500);
+            }else {
+                if (vidasRestantes < 2) {
+                    vidas.style.color = "red";
+                } else if (vidasRestantes < 3) {
+                    vidas.style.color = "yellow";
+                } else if (vidasRestantes < 4) {
+                    vidas.style.color = "orange";
+                }
+            setTimeout(() => {
+                cartaClicada1.classList.remove('clicada');
+                cartaClicada2.classList.remove('clicada');
+                cartaClicada1.classList.add('oculta');
+                cartaClicada2.classList.add('oculta');
+                cartaClicada1 = null;
+            }, 1000);
+            }
+            vidas.innerText = vidasRestantes + " vidas";
+        }
+    }
+}
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+function generarCartas(numCartas){
     let cartasMostrar = new Array(numCartas*2);
     for(let i = 0; i < numCartas; i++){
         let numRandom = getRandomIntInclusive(0, cartas.length-1);
@@ -74,25 +144,17 @@ window, addEventListener('load', () => {
         }
         cartasMostrar[pos2] = cartas[numRandom];
     }
+    return cartasMostrar;
+}
+function imprimirCartas(cartasMostrar) {
+    let body = document.querySelector('body');
     cartasMostrar.forEach(carta => {
-        body.innerHTML += `<div class="carta oculta" id="${carta.nombre}" style="background-position: ${carta.pos1}px -${carta.pos2}px ;"></div>`;
+        body.innerHTML += `<div class="carta oculta" data-carta="${carta.nombre}" style="background-position: ${carta.pos1}px -${carta.pos2}px ;"></div>`;
     });
     let cartasCreadas = document.querySelectorAll('.carta');
     cartasCreadas.forEach(carta=>{
-        carta.addEventListener('mouseenter', hover);
-    })
-    function hover(event){
-        event.target.classList.add('hover')
-        event.target.classList.remove('oculta')
-        event.target.removeEventListener('mouseenter', hover);
-    }
-});
-
-
-
-function getRandomIntInclusive(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        carta.addEventListener('mousedown', click);
+    });
 }
+
 
